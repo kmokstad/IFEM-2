168f84596792b10bb113b448100ea59b28a17843
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-08-27 11:19:34 +0200

    Added: Additional MADOF support in SIMbase for mixed problems.
    This allows DOF vectors with an "unusual" number of DOFs on a given basis.
    Added usage of this in the HDF5Writer for such fields.

--- src/SIM/SIMbase.h
+++ src/SIM/SIMbase.h
@@ -228,7 +228,7 @@ public:
   //! \brief Returns the number of right-hand-side vectors.
   virtual size_t getNoRHS() const;
   //! \brief Returns the number of bases in the model.
-  char getNoBasis() const;
+  unsigned char getNoBasis() const;
 
   //! \brief Returns the type (DOF classification) of the specified global node.
   char getNodeType(int inod) const;
@@ -588,17 +588,21 @@ public:
   //! \param[out] vec Local solution vector associated with specified patch
   //! \param[in] pindx Local patch index to extract solution vector for
   //! \param[in] nndof Number of DOFs per node (optional)
+  //! \param[in] basis Basis to extract for (optional)
   //! \return Total number of DOFs in the patch (first basis only if mixed)
   size_t extractPatchSolution(const Vector& sol, Vector& vec, int pindx,
-                              unsigned char nndof = 0) const;
+                              unsigned char nndof = 0,
+                              unsigned char basis = 0) const;
 
   //! \brief Injects a patch-wise solution vector into the global vector.
   //! \param sol Global primary solution vector in DOF-order
   //! \param[in] vec Local solution vector associated with specified patch
   //! \param[in] pindx Local patch index to inject solution vector for
   //! \param[in] nndof Number of DOFs per node (optional)
+  //! \param[in] basis Basis to inject for (optional)
   bool injectPatchSolution(Vector& sol, const Vector& vec, int pindx,
-                           unsigned char nndof = 0) const;
+                           unsigned char nndof = 0,
+                           unsigned char basis = 0) const;
 
   //! \brief Extracts element results for a specified patch.
   //! \param[in] glbRes Global element result array
@@ -751,6 +755,15 @@ protected:
 private:
   size_t nIntGP; //!< Number of interior integration points in the whole model
   size_t nBouGP; //!< Number of boundary integration points in the whole model
+
+  //! \brief Setup a MADOF with an extraordinary amount of DOFs on a basis.
+  //! \param[in] basis The basis to specify number of DOFs for
+  //! \param[in] nndof Number of DOFs on given basis
+  //! \param[out] madof Generated MADOF array
+  void setupAdditionalMADOF(unsigned char basis, unsigned char nndof,
+                            std::vector<int>& madof) const;
+
+  mutable std::map<int, std::vector<int> > addMADOFs; //!< Additional MADOF arrays.
 };
 
 #endif
